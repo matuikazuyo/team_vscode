28cd9a39a9ee8fe7557f7c727d4799ccaa35e885
fujikawa yuhi <y.fujikawa.sys20@morijyobi.ac.jp>
2021-03-30 11:51:27 +0900

    円の計算を追加
    en.htmlとen2.htmlの追加

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,7 +6,16 @@ app = Flask(__name__)
 def top():
     return render_template("index.html")
 
+@app.route("/en")
+def en():
+    return render_template("en.html")
 
+@app.route("/en_calc")
+def en_calc():
+    num = request.args.get("num")
+    ensyu = num * 2 * 3.14
+    menseki = num * num * 3.14
+    return render_template("en2.html", ensyu=ensyu, menseki=menseki)
 
 if __name__ = "__main__":
     app.run(debug=True)
